a6b172eec15278f589bcc06d770520b1f7cfe4e8
Victor <b3m.v@yandex.ru>
2023-09-19 00:02:08 +0300

    Feat: Настроен Storyboard в соответсвии с техническим заданием.

--- Counter/ViewController.swift
+++ Counter/ViewController.swift
@@ -8,12 +8,31 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var counterScoreLabel: UILabel!
+    
+    private var counter: Int = 0 {
+        didSet{
+            counterScoreLabel.text = "Значение счетчика:\n/(counter)"
+        }
+    }
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    //Метод для обработки нажания на кнопку увеличения счетчика
+    @IBAction func increaseButtonTapped(_ sender: UIButton) {
+        counter += 1
+    }
+    
+    //Метод для обработки нажания на кнопку уменьшения счетчика
+    @IBAction func decreaseButtonTapped(_ sender: UIButton) {
+        if counter >= 1 {
+            counter -= 1
+        }
+    }
+    
 }
 
